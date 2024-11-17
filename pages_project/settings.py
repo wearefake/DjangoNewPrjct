2fc33b7b3c860b5bdbdb66ca63357c4d4582adbc
wearefake <wearefake@yandex.ru>
2024-11-17 18:32:43 +0300

    Changed debug mode

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -21,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^r0yvvd$rl^pqh+-=+b3u!%io)(-32&@_be7yd#xjzc@o2-s(s'
+# SECRET_KEY = 'django-insecure-^r0yvvd$rl^pqh+-=+b3u!%io)(-32&@_be7yd#xjzc@o2-s(s'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
